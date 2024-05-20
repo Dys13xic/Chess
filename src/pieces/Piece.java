44c44316fe125d4951c54f627ae6a42d83a60cfb
Dys13xic <campbell.leckie@hotmail.com>
2024-05-20 00:44:51 -0400

    added Colour and Type enum, implemented getter methods

--- src/pieces/Piece.java
+++ src/pieces/Piece.java
@@ -1,8 +1,38 @@
 package pieces;
+
 public abstract class Piece {
+    public enum Colour {
+        WHITE,
+        BLACK
+    }
+    public enum Type {
+        PAWN,
+        ROOK,
+        KNIGHT,
+        BISHOP,
+        QUEEN,
+        KING
+    }
     // TODO determine if Piece needs to be included in pieces package
-    boolean colour;
-    int row;
-    int column;
+    private Colour colour;
+    private Type type;
+    private int rank;
+    private int file;
+
+    public Colour getColour() {
+        return colour;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public int getFile() {
+        return file;
+    }
     
 }
