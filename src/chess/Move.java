975cf53196d233c296f0a66efe08e5bc674ff31f
Dys13xic <campbell.leckie@hotmail.com>
2026-01-13 22:40:12 -0500

    Introduce regex for validating chess notation.

--- src/chess/Move.java
+++ src/chess/Move.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 import chess.pieces.Piece;
 
 public class Move {
@@ -11,16 +14,40 @@ public class Move {
         RESULT
     }
 
+    private static final Pattern MOVE_PATTERN = Pattern.compile("^(((?<piece>[NBRQK])?(?<rank>[a-h])?(?<file>[1-8])?(?<capture>x)?(?<rankAndFile>[a-h][1-8])(?<promote>=[NBRQ])?)|(?<castle>O-O(-O)?))(?<check>[+#])?$");
+
     int move;
     Type type;
-    Piece selected;
-    Piece captured;
-    Piece promotedTo;
+    int sourceRank;
+    int sourceFile;
+    int targetRank;
+    int targetFile;
+    Piece.Type selected;
+    Piece.Type promotedTo;
+    boolean capture;
     boolean check;
     boolean mate;
     String notation;
 
-    public Move(String text) {
+    public Move(String notation) {
+        if (notation.equals("=")) {
+            type = Type.OFFER_DRAW; 
+        }
+        else {
+            Matcher matcher = MOVE_PATTERN.matcher(notation);
 
+            String castle = matcher.group("castle");
+            String checkType = matcher.group("check");
+
+
+            final int hFile = 7;
+            final int aFile = 0;
+            if (castle.startsWith("O-O-O")) {
+                sourceFile = hFile;
+            }
+            else if (castle.startsWith("O-O")) {
+                sourceFile = aFile;
+            }
+        }
     }
 }
