ace5ca45aeb3a1ef39462b4d05153ec8f454ccc3
Dys13xic <campbell.leckie@hotmail.com>
2026-03-28 09:22:51 -0400

    Continue implementing loadPosition(). Move parsing of FEN into constructor.

--- src/chess/positionformats/Fen.java
+++ src/chess/positionformats/Fen.java
@@ -1,5 +1,6 @@
 package chess.positionformats;
 
+import chess.Coordinate;
 import chess.Move;
 import chess.pieces.Bishop;
 import chess.pieces.King;
@@ -13,132 +14,123 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
-// TODO This class is very rough, clean it up!
 public class Fen implements PositionFormats{
     String fen;
+    Piece.Colour activePlayer;
+    int halfMoveClock;
+    int moveCount;
 
     public Fen(String notation) {
-        fen = notation;
-    }
-
-    public void loadPosition(chess.Board chessBoard) {
         // Validate FEN structure
-        if (!fen.matches("^([rnbqkpRNBQKP1-8]{1,8}\\/){7}[rnbqkpRNBQKP1-8]{1,8}\\s[w,b]\\s([kqKQ]{1,4}|-)\\s(([a-h][3,6])|-)\\s\\d{1,3}\\s\\d{1,3}$")) {
-            // throw new IllegalArgumentException();
+        if (!notation.matches("^([rnbqkpRNBQKP1-8]{1,8}\\/){7}[rnbqkpRNBQKP1-8]{1,8}\\s[w,b]\\s([kqKQ]{1,4}|-)\\s(([a-h][3,6])|-)\\s\\d{1,3}\\s\\d{1,3}$")) {
+            // TODO throw exception
         }
+        fen = notation;
 
         String[] fields = fen.split(" ");
+        activePlayer = fields[1].equals("w") ? Piece.Colour.WHITE : Piece.Colour.BLACK;
+        halfMoveClock = Integer.parseInt(fields[4]);
+        moveCount = Integer.parseInt(fields[5]);
+    }
 
-        // 1 - Position
+    public void loadPosition(chess.Board board) {
+        String[] fields = fen.split(" ");
+        board.clear();
+
         String[] position = fields[0].split("/");
-        Collections.reverse(Arrays.asList(position));           // TODO should I replace this with more handwritten code?
+        Collections.reverse(Arrays.asList(position));
 
         for (int rank = 0; rank < position.length; rank++) {
-            int rankSquareCount = 0;
-            for (int file = 0; file < position[rank].length(); file++) {
+            int file = 0;
+            for (int i = 0; i < position[rank].length(); i++) {
+                char currentCharacter = position[rank].charAt(i);
+                Piece.Colour colour = Character.isUpperCase(currentCharacter) ? Piece.Colour.WHITE : Piece.Colour.BLACK;
                 Piece currentPiece;
-                Piece.Colour colour = Piece.Colour.WHITE;
-                switch(position[rank].charAt(file)) {
+
+                switch(currentCharacter) {
                     case 'p':
-                        colour = Piece.Colour.BLACK;
                     case 'P':
-                        currentPiece = new Pawn(colour, false); // TODO actually determine enpassant
-                        chessBoard.insertPiece(rank, file, currentPiece);
-                        rankSquareCount++;
+                        currentPiece = new Pawn(colour, false);
                         break;
 
                     case 'r':
-                        colour = Piece.Colour.BLACK;
                     case 'R':
                         currentPiece = new Rook(colour);
-                        chessBoard.insertPiece(rank, file, currentPiece);
-                        rankSquareCount++;
                         break;
 
                     case 'n':
-                        colour = Piece.Colour.BLACK;
                     case 'N':
                         currentPiece = new Knight(colour);
-                        chessBoard.insertPiece(rank, file, currentPiece);
-                        rankSquareCount++;
                         break;
 
                     case 'b':
-                        colour = Piece.Colour.BLACK;
                     case 'B':
                         currentPiece = new Bishop(colour);
-                        chessBoard.insertPiece(rank, file, currentPiece);
-                        rankSquareCount++;
                         break;
 
                     case 'q':
-                        colour = Piece.Colour.BLACK;
                     case 'Q':
                         currentPiece = new Queen(colour);
-                        chessBoard.insertPiece(rank, file, currentPiece);
-                        rankSquareCount++;
                         break;
 
                     case 'k':
-                        colour = Piece.Colour.BLACK;
                     case 'K':
-                        currentPiece = new King(colour, false); // TODO determine if in check
-                        chessBoard.insertPiece(rank, file, currentPiece);
-                        rankSquareCount++;
+                        currentPiece = new King(colour, false, false);
                         break;
 
                     default:
                         char character = position[rank].charAt(file);
-                        if (character < '0' || character > '9') {
-                            // TODO throw error
-                        }
                         int emptySquares = character - '0';
-                        
-                        if (file + emptySquares < chessBoard.getFileCount()) {
-                            for (int i = 0; i < emptySquares; i++) {
-                                chessBoard.insertPiece(rank, file + i, null);
-                                rankSquareCount++;
-                            }
-                        }
-                        else {
-                            // Throw exception
-                        }
-
-                        break;
+                        file += emptySquares;
+                        continue;
                 }
-            }
-            if (rankSquareCount != chessBoard.getRankCount()) {
-                throw new IllegalArgumentException();
+                board.insertPiece(rank, file, currentPiece);
+                file++;
             }
         }
+ 
+        // TODO complete castle rights!
+        String castleRights = fields[2];
+        for (int i = 0; i < castleRights.length(); i++) {
+            // switch(castleRight.charAt(i)) {
+            //     case: 'w'
+            //         board.getP
+                    
+            // }
+        }
 
-        // TODO 3 - Castle Rights
-        // If king has moved or rook has moved Castle Rights must accurately reflect that.
+        String enPassantTarget = fields[3];
+        if (!enPassantTarget.equals("-")) {
+            Coordinate enPassantSquare = new Coordinate(enPassantTarget);
+            Piece.Colour boardHalf = board.getHalf(enPassantSquare);
+            int directionOfTravel = boardHalf == Piece.Colour.WHITE ? 1 : -1;
+            
+            Coordinate targetSquare = new Coordinate(enPassantSquare.getRank() + directionOfTravel, enPassantSquare.getFile());
+            Piece targetPiece = board.getPieceAt(targetSquare);
+            if (Pawn.class.isInstance(targetPiece)) {
+                Pawn targetPawn = Pawn.class.cast(targetPiece);
+                targetPawn.setEnPassantTarget(true);
+            }
+        }
+    }
 
-        // Ensure there is only one king for each side
+    public Piece.Colour getActivePlayer() {
+        return activePlayer;
+    }
 
-        // TODO 4 - En Passant
+    public int getHalfMoveClock() {
+        return halfMoveClock;
     }
 
     public ArrayList<Move> getMoves() {
-        String[] fields = fen.split(" ");
-        int moveCount = Integer.parseInt(fields[5]);
         ArrayList<Move> moveList = new ArrayList<Move>();
         for (int i = 0; i < moveCount; i++) {
             moveList.add(null);
         }
         return moveList;
     }
-    public Piece.Colour getActivePlayer() {
-        String[] fields = fen.split(" ");
-        return fields[1].equals("w") ? Piece.Colour.WHITE : Piece.Colour.BLACK;
-    }
-    public int getHalfMoveClock() {
-        String[] fields = fen.split(" ");
-        return Integer.parseInt(fields[4]);
-    }
 
-    public String export(chess.Board chessBoard) {
+    public String export(chess.Board board) {
         return "";
     }
 }
